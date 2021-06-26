bd2fc4621886c49a2e5927a4524e96309741696f
Paul Ranshaw <p.s.ranshaw@gmail.com>
2021-06-27 00:22:33 +0100

    Update embed.py

--- cogs/embed.py
+++ cogs/embed.py
@@ -7,8 +7,10 @@ class Embed(commands.Cog):
     def __innit__(self, bot):
         self.bot = bot
 
-    async def embed(self, title, description, channel):
-        embed = discord.Embed(title=title, description=description, colour=0xe6f5ff, timestamp=datetime.datetime.utcnow())
+    async def embed(self, title, description, colour, channel):
+        if colour is None:
+            colour = 0xe6f5ff
+        embed = discord.Embed(title=title, description=description, colour=colour, timestamp=datetime.datetime.utcnow())
         await channel.send(embed=embed)
 
 def setup(bot):
