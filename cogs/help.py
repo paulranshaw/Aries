806043c5b57a077c6d7ac3f53d1b65e1816293b8
Paul Ranshaw <p.s.ranshaw@gmail.com>
2021-07-01 12:43:55 +0100

    Create help.py

---
+++ cogs/help.py
@@ -0,0 +1,17 @@
+from cogs.embed import *
+from discord.ext import commands
+
+class Help(commands.Cog):
+
+    def __innit__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    @commands.guild_only()
+    async def help(self, ctx):
+        await Embed.embed(self, "Help", "Checkout **Aries** documentation at: [Help](https://github.com/paulranshaw/aries/blob/master/README.md)", None, ctx.channel)
+
+def setup(bot):
+    bot.remove_command('help')
+    bot.add_cog(Help(bot))
