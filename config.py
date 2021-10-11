2785bc64f3a550376fafb3d82b85e5e99154e8eb
Paul Ranshaw <p.s.ranshaw@gmail.com>
2021-10-11 11:32:30 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 token = ''
 prefix = ['a!']
-cogs = ['cogs.avatar', 'cogs.ban', 'cogs.command_prefix', 'cogs.embed', 'cogs.GIF', 'cogs.help', 'cogs.invite', 'cogs.kick', 'cogs.myanimelist', 'cogs.mute', 'cogs.poll', 'cogs.purge', 'cogs.vote', 'cogs.unban', 'cogs.unmute']
+cogs = ['cogs.avatar', 'cogs.ban', 'cogs.command_prefix', 'cogs.embed', 'cogs.GIF', 'cogs.help', 'cogs.invite', 'cogs.kick', 'cogs.myanimelist', 'cogs.mute', 'cogs.poll', 'cogs.profile', 'cogs.purge', 'cogs.vote', 'cogs.unban', 'cogs.unmute']
